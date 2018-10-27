4667ea577685b6500533ea1ad1cdfb51ee579ffa
Kanogaelias <kanogae@gmail.com>
2018-10-27 19:56:06 +0300

    add component selector

--- src/app/search-profile/search-profile.component.ts
+++ src/app/search-profile/search-profile.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { SearchProfilesService } from '../search-profile.service';
 
 @Component({
-  selector: 'app-search-users',
+  selector: 'app-search-profile',
   templateUrl: './search-profile.component.html',
   styleUrls: ['./search-profile.component.css']
 })
-export class SearchUsersComponent implements OnInit {
+export class SearchProfileComponent implements OnInit {
   place: string;
 
   results: any[] = []; // Store data in results from the service
