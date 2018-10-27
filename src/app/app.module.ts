7c297d5da6239df53186800052ce89819b378371
Kanogaelias <kanogae@gmail.com>
2018-10-27 19:45:06 +0300

    add component search-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchProfileComponent } from './search-profile/search-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchProfileComponent
   ],
   imports: [
     BrowserModule
