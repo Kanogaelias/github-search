5078db146ed9d6b919bdf13848c20665ce568010
Kanogaelias <kanogae@gmail.com>
2018-10-27 20:15:22 +0300

    add providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { SearchProfileComponent } from './search-profile/search-profile.component';
+import { SearchProfilesService } from './search-profiles.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,11 @@ import { SearchProfileComponent } from './search-profile/search-profile.componen
     SearchProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [SearchProfilesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
